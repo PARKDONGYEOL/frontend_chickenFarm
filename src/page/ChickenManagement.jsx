4c42180bd1c9d909b8303e0a24a8423864fb391c
Taeee372 <parktk99@gmail.com>
2025-10-01 16:43:41 +0900

    ChickenManagement.jsx에서 배치 등록 기능 완료

--- src/page/ChickenManagement.jsx
+++ src/page/ChickenManagement.jsx
@@ -15,16 +15,41 @@ const ChickenManagement = () => {
     'initialCount' : ''
   })
 
-  //축사 등록
+  //양계장 등록
   const regFarmName = () => {
     axios.post(`/api/farm`, {farmName : farmName})
-    .then(res => alert('등록 완료'))
+    .then(res => {
+      alert('등록 완료');
+      setFarmName('');
+    })
     .catch(e => {
       console.log(e)
     });
   }
 
-  //배치 등록
+  //배치 & 개체 동시 등록
+  const regBatchAndChickens = () => {
+    axios.post('/api/batch', batch)
+    .then(res => {
+      alert('등록 완료');
+      setBatch({
+        'farmNum' : '',
+        'entryDate' : '',
+        'initialCount' : ''
+      })
+    })
+    .catch(e => {
+      console.log(e)
+    });
+  }
+
+  //배치 인풋에 입력한 값으로 변경
+  const handleBatch = (e) => {
+    setBatch({
+      ...batch,
+      [e.target.name] : e.target.value
+    })
+  }
 
   return (
     <div className={styles.container}>
@@ -34,19 +59,32 @@ const ChickenManagement = () => {
         <div>
           <span>양계장 이름</span>
           <Input value={farmName} onChange={(e) => {setFarmName(e.target.value)}}/>
-          <Button title='등록' onClick={() => {regFarmName()}}/>
+          <Button title='등록' onClick={() => regFarmName()}/>
         </div>
       </div>
       <div>
         <h3>배치 등록</h3>
         <div>
-          <span>축사 번호</span>
-          <Input />
+          <span>양계장 번호</span>
+          <Input 
+            name='farmNum'
+            value={batch.farmNum}
+            onChange={(e) => handleBatch(e)}
+          />
           <span>입식일</span>
-          <Input />
+          <Input 
+            type='date'
+            name='entryDate'
+            value={batch.entryDate}
+            onChange={(e) => handleBatch(e)}
+          />
           <span>닭의 수</span>
-          <Input />
-          <Button title='등록'/>
+          <Input 
+            name='initialCount'
+            value={batch.initialCount}
+            onChange={(e) => handleBatch(e)}
+          />
+          <Button title='등록' onClick={() => regBatchAndChickens()}/>
         </div>
       </div>
       <div>
